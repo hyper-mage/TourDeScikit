1e662fffd3831772e91821e6497a48d1a59565af
hyper-mage <mlynn808138@gmail.com>
2019-06-23 11:16:38 -0500

    Continue through Model persistence

--- Documentation Examples.py
+++ Documentation Examples.py
@@ -21,3 +21,22 @@ svm.SVC(C=100.0, cache_size=200, class_weight=None, coef0=0.0, decision_function
 print(clf.predict(digits.data[-1:]))
 
 # next lets work on some model persistence, or mnist data.
+
+# We proceed with model persistence, lets finish this tutorial and move on to personal projects afterwards
+
+# Model Persistence
+# we utilize python's built in serializer, pickle. Pickle basically bytes and unbytes objects
+
+clf = svm.SVC(gamma = 'scale')
+iris = datasets.load_iris()
+X, y = iris.data, iris.target  # that is super dope that i can assign variables like this
+clf.fit(X, y)
+svm.SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0, decision_function_shape='ovr',
+        degree=3, gamma='scale', kernel='rbf', max_iter=-1, probability=False, random_state=None,
+        shrinking=True, tol=0.001, verbose=False)
+
+import pickle
+s = pickle.dumps(clf)
+clf2 = pickle.loads(s)
+clf2.predict(X[0:1])
+y[0]
