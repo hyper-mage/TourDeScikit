b282eaa7a283509cae67404925f6e418f84b1ee4
hyper-mage <mlynn808138@gmail.com>
2019-06-24 06:32:31 -0500

    Print outputs and add joblib import

--- Documentation Examples.py
+++ Documentation Examples.py
@@ -15,9 +15,9 @@ clf = svm.SVC(gamma=0.001, C=100)
 # [:-1] produces an array that contains all but the last item from the attached data
 
 clf.fit(digits.data[:-1], digits.target[:-1])
-svm.SVC(C=100.0, cache_size=200, class_weight=None, coef0=0.0, decision_function_shape='ovr',
-        degree=3, gamma=0.001, kernel='rbf', max_iter=-1, probability=False, random_state=None,
-        shrinking=True, tol=0.001, verbose=False)
+# svm.SVC(C=100.0, cache_size=200, class_weight=None, coef0=0.0, decision_function_shape='ovr',
+#         degree=3, gamma=0.001, kernel='rbf', max_iter=-1, probability=False, random_state=None,
+#         shrinking=True, tol=0.001, verbose=False)
 print(clf.predict(digits.data[-1:]))
 
 # next lets work on some model persistence, or mnist data.
@@ -27,16 +27,26 @@ print(clf.predict(digits.data[-1:]))
 # Model Persistence
 # we utilize python's built in serializer, pickle. Pickle basically bytes and unbytes objects
 
-clf = svm.SVC(gamma = 'scale')
+clf = svm.SVC(gamma='scale')
 iris = datasets.load_iris()
 X, y = iris.data, iris.target  # that is super dope that i can assign variables like this
 clf.fit(X, y)
-svm.SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0, decision_function_shape='ovr',
-        degree=3, gamma='scale', kernel='rbf', max_iter=-1, probability=False, random_state=None,
-        shrinking=True, tol=0.001, verbose=False)
+# svm.SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0, decision_function_shape='ovr',
+#         degree=3, gamma='scale', kernel='rbf', max_iter=-1, probability=False, random_state=None,
+#         shrinking=True, tol=0.001, verbose=False)
 
 import pickle
 s = pickle.dumps(clf)
 clf2 = pickle.loads(s)
-clf2.predict(X[0:1])
-y[0]
+print(clf2.predict(X[0:1]))
+print(y[0])
+
+# Use joblib, a "big data" version of pickle
+
+from joblib import load, dump
+dump(clf, 'filename.joblib')
+
+clf = load('filename.joblib')
+
+# The goal of joblib is to save models for later use
+
